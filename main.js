177bd6e53009c0d805469a8738f227dcfcb0e494
spiridon88 <lebedevda123@mail.ru>
2020-05-13 18:00:13 +0300

    1-я домашняя работа

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-
+var name = prompt ('Ваше имя ?');
+str1 = 'Привет, мир! Меня зовут ';
+alert (str1 + name);
+var a = 4;
+var b = 3;
+var c = 5;
+var s = (a+b+c)/3;
+alert ('Алгебра: ' +a);
+alert ('Физика: ' +b);
+alert ('Георафия: ' +c);
+alert ('Средняя оценка: ' +s);
+ 
